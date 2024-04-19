bf7e43c25ea0ffa04dfb4d4184262a0ab921196e
Javier Andres Solarte <79766563+dresandev@users.noreply.github.com>
2024-04-19 00:03:30 -0500

    fix: projects description

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,7 +4,7 @@ import { Tag } from './tags'
 export const projects: Project[] = [
   {
     name: 'Dreflix',
-    description: 'Sitio web dedicado a explorar el mundo cinematográfico, donde descubrirás información sobre una gran variedad de películas.Aquí podrás acceder a detalles como la sinopsis, el tráiler, el reparto principal entre otros detalles.Su diseño es una mezcla del diseño de distintas plataformas de transmisión de video como Amazon Prime y Crunchyroll.',
+    description: 'Sitio web dedicado a explorar el mundo cinematográfico, donde descubrirás información sobre una gran variedad de películas. Aquí podrás acceder a detalles como la sinopsis, el tráiler, el reparto principal entre otros detalles. Su diseño es una mezcla del diseño de distintas plataformas de transmisión de video como Amazon Prime y Crunchyroll.',
     tags: [
       Tag.nextJS14,
       Tag.cssModules,
@@ -25,7 +25,7 @@ export const projects: Project[] = [
   },
   {
     name: 'Vexara',
-    description: 'Clon de Bershka implementando las funcionalidades principales como flujo de autenticación usando auth.js, flujo de pago de productos usando mercadopago, implementando código que funciona para producción pero que al ser un proyecto de practica todo va a seguir en modo de desarrollo.',
+    description: 'Clon de Bershka implementando las funcionalidades principales como el flujo de autenticación usando auth.js, el flujo de pago de productos usando la pasarela de pagos mercadopago, implementando código funcional en producción pero que al ser un proyecto de practica va a seguir en modo de desarrollo.',
     tags: [
       Tag.mercadopago,
       Tag.zustand,
