2bf6c0d0a6c0e7ca52dd65fab2a855d70a76e355
Javier Andres Solarte <79766563+dresandev@users.noreply.github.com>
2024-04-20 16:22:48 -0500

    fix: base and site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://dresan.dev.github.io',
-  // base: '/dresan.dev',
+  site: 'https://dresan.github.io',
+  base: '/dresan.dev',
 });
