7e085fe2fd5d61ea342c49fab559177b9ead8118
Javier Andres Solarte <79766563+dresandev@users.noreply.github.com>
2024-04-20 23:49:34 -0500

    feat: mobile nav link close menu onClick

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+export const NAV_LINKS = [
+  {
+    label: "Inicio",
+    url: "#inicio",
+  },
+  {
+    label: "Proyectos",
+    url: "#proyectos",
+  },
+  {
+    label: "Sobre mí",
+    url: "#sobre-mí",
+  },
+  {
+    label: "Links",
+    url: "#links",
+  },
+]
